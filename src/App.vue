47db769235447e39317eea087b8a27a454a3b14c
leandromccordeiro <leandroo_marcos@yahoo.com.br>
2024-09-17 11:18:04 -0300

    componente com props

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@ import ComDados from "./components/ComDados.vue"
 import ComDadosReativosPrimitivos from "./components/ComDadosReativosPrimitivos.vue";
 import ComDadosReativosNaoPrimitivos from "./components/ComDadosReativosNaoPrimitivos.vue";
 import DataBinding from "./components/DataBinding.vue";
+import ComProps from "./components/ComProps.vue";
 </script>
 
 <template>
   <header>Vue</header>
+  <ComProps item="Borracha" :preco="2.50"/>
   <DataBinding/>
   <ComDadosReativosNaoPrimitivos/>
   <ComDadosReativosPrimitivos/>
